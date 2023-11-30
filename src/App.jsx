95e4a8532107cc299ae4efb8032c1538a031fc58
juanrabaro <jrbaro@iesrafaelalberti.es>
2023-11-30 10:43:54 +0100

    todo subido

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import FormNoControlado from "./FormNoControlado"
+
+function App() {
+
+return(
+    <div className="container">
+        <h1>Formulario controlado</h1>
+        <FormNoControlado />
+    </div>
+)
+}
+
+export default App
